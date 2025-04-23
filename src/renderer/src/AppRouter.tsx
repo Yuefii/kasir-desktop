af5fcc9f41fa43d9e913c5bc6306a04c00edf611
Yuefii <muffidahmadiawan31@gmail.com>
2025-04-23 22:01:28 +0700

    chore: fix router untuk prod

--- src/renderer/src/AppRouter.tsx
+++ src/renderer/src/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createHashRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './layouts/AppLayout'
 import Kasir from './views/Kasir'
 import Product from './views/Product'
@@ -7,49 +7,27 @@ import User from './views/User'
 import Branch from './views/Branch'
 import EntriStock from './views/EntriStock'
 import RegisterStock from './views/RegisterStock'
-import { ReactElement } from 'react'
 
-const router = createBrowserRouter([
+const routes = [
   {
     path: '/',
     element: <Layout />,
+    errorElement: <div>404 Not Found</div>,
     children: [
-      {
-        index: true,
-        element: <Kasir />
-      },
-      {
-        path: 'produk',
-        element: <Product />
-      },
-      {
-        path: 'kategori',
-        element: <Category />
-      },
-      {
-        path: 'pegawai',
-        element: <User />
-      },
-      {
-        path: 'cabang',
-        element: <Branch />
-      },
-      {
-        path: 'entri-stok',
-        element: <EntriStock />
-      },
-      {
-        path: 'daftar-stok',
-        element: <RegisterStock />
-      },
-      {
-        path: 'entri-stok',
-        element: <EntriStock />
-      }
+      { index: true, element: <Kasir /> },
+      { path: 'produk', element: <Product /> },
+      { path: 'kategori', element: <Category /> },
+      { path: 'pegawai', element: <User /> },
+      { path: 'cabang', element: <Branch /> },
+      { path: 'entri-stok', element: <EntriStock /> },
+      { path: 'daftar-stok', element: <RegisterStock /> }
     ]
   }
-])
+]
 
-export default function Router(): ReactElement {
+const router =
+  import.meta.env.MODE === 'development' ? createBrowserRouter(routes) : createHashRouter(routes) // 👈 penting di production
+
+export default function Router() {
   return <RouterProvider router={router} />
 }
