71f76fe3baef6ff2f5156da5fe75975464a4e510
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-05 12:54:44 +0700

    note: memberikan komentar untuk memberitahu fungsi code pada check_connection

--- src/shared/helper/check_connection.ts
+++ src/shared/helper/check_connection.ts
@@ -1,11 +1,17 @@
 import dns from 'dns'
 
+//  NOTE:
+// fungsi untuk pengecekan perangkat sedang terhubung ke internet atau tidak.
 export function isConnectedToInternet() {
   return new Promise((resolve) => {
     dns.lookup('google.com', (err) => {
       if (err && err.code === 'ENOTFOUND') {
+        //  NOTE:
+        //  jika domain tidak bisa diresolve, maka dianggap koneksi tidak tersedia.
         resolve(false)
       } else {
+        //  NOTE:
+        //  jika berhasil diresolve, maka koneksi internet tersedia.
         resolve(true)
       }
     })
