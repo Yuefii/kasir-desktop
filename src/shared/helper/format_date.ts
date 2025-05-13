8216ade08bfdb06b8fe794c3dbbf13b1d1b545e9
Yuefii <muffidahmadiawan31@gmail.com>
2025-05-13 22:51:47 +0700

    chore: menambahkan global helper untuk format rupiah dan tanggal

---
+++ src/shared/helper/format_date.ts
@@ -0,0 +1,10 @@
+export const formatDateTime = (date: Date) => {
+  return new Intl.DateTimeFormat('id-ID', {
+    day: '2-digit',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }).format(new Date(date))
+}
